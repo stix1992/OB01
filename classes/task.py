9e2e37014cdcf09e951bcc42c7862099af976383
stix1992 <stix1992@mail.ru>
2024-06-05 23:04:01 +0300

    final edition 33

--- classes/task.py
+++ classes/task.py
@@ -3,7 +3,7 @@ class Task:
         self.task_list = []
 
     def add_task(self, description, due_date):
-        self.task_list.append({'description':description, 'due_date':due_date, 'completed':False})
+        self.task_list.append({'description': description, 'due_date': due_date, 'completed': False})
         print(f'Задача "{description}" добавлена!')
 
     def mark_completed(self, description):
@@ -16,6 +16,5 @@ class Task:
             print(f'Задача "{description}" не найдена!')
 
     def __str__(self):
-        return '\nCписок задач:\n' + '\n'.join([f'   {task["description"]} - выполнить до {task["due_date"]}' for task in self.task_list if not task['completed']])
-
-
+        return '\nCписок задач:\n' + '\n'.join([f'   {task["description"]} - выполнить до {task["due_date"]}'
+                                                for task in self.task_list if not task['completed']])
